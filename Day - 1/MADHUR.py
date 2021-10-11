dc2cc0f51d98643cf68139ef7f989ebbb9d0a55e
madhur-28 <92255952+madhur-28@users.noreply.github.com>
2021-10-11 23:37:08 +0530

    MADHUR.py
    day 1

---
+++ Day - 1/MADHUR.py
@@ -0,0 +1,18 @@
+# kangaroo problem
+
+t = int(input())
+while (t>0):
+    n = int(input())
+    i = 0
+    if (i==n):
+        print("YES")
+        print("NO")
+    else:
+        i=1
+        while (i<n):
+            i = i + i
+        if(i==n):
+            print("YES")
+        else:
+            print("NO")
+    t = t - 1
