a533fe067b0b37923cb6ae55bc829efc6469a721
subham singh rawat <67608747+SubhamSRawat@users.noreply.github.com>
2021-10-11 12:55:54 +0530

    Add files via upload

---
+++ Day - 1/subham-rawat.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        if((n&(n-1)) == 0) cout<<"YES";
+        else cout<<"NO";
+        cout<<endl;
+    }
+
+    return 0;
+}
