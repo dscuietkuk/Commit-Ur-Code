24ec49c7767bae50b5763f92d4e08a7ce5cfe087
Rahulsainitech <66263277+Rahulsainitech@users.noreply.github.com>
2021-10-11 22:57:16 +0530

    Create Rahul_1.py

---
+++ day-1/Rahul_1.py
@@ -0,0 +1,15 @@
+
+def jump(N):
+     if N==1:
+        return 'YES'
+     while N>2:
+        if N%2==0:
+            N=N/2
+        else:
+            return 'NO'
+     return 'YES'
+
+t=int(input())
+for _ in range(t):
+   N=int(input())
+   print(jump(N))
