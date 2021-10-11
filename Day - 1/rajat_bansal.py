306ff527c193d06daddd573499772c64dced7f89
Rajat <rajatbansal884@gmail.com>
2021-10-11 16:21:17 +0530

    Day 1 by Rajat Bansal

---
+++ Day - 1/rajat_bansal.py
@@ -0,0 +1,15 @@
+import math
+
+
+def can_reach(x):
+    if x in (0, 1):
+        return True
+    return math.log2(x) == int(math.log2(x))
+
+
+for test_case in range(int(input())):
+    N = int(input())
+    if can_reach(N):
+        print("Yes")
+    else:
+        print("No")
