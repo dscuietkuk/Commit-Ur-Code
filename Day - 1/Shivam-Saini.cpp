edfee0ff947d6c218b2fdbdc7945088c37a6f734
ShivamSaini09 <92246594+ShivamSaini09@users.noreply.github.com>
2021-10-11 16:58:34 +0530

    Update Shivam-Saini.cpp

--- Day - 1/Shivam-Saini.cpp
+++ Day - 1/Shivam-Saini.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 int main()
 {
-    int t, n;
+    int t;
+    long long int n;
     cin >> t;
     for (int i = 0; i < t; i++)
     {
         cin >> n;
-        int x = 1;
+        long long int x = 1;
         while (x < n)
         {
             x += x;
         }
-        if (x == n || n == 0)
+        if (x == n )
         {
             cout << "YES" << endl;
         }
