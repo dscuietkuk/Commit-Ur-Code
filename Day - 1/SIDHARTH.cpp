f04996fc4c59e3d8d6bb63621a10d0954b543890
00-00-0000 <92293535+00-00-0000@users.noreply.github.com>
2021-10-11 12:46:02 +0530

    Create SIDHARTH.cpp
    SUBMIT DAY 1

---
+++ Day - 1/SIDHARTH.cpp
@@ -0,0 +1,29 @@
+/*
+ Author: 	SIDHARTH
+ Module: 	day -1
+ Topic: 	kangarro problem
+ N=DISTANCE
+*/  
+#include<bits/stdc++.h>
+using namespace std;
+  
+// Function to check if N is power of 2
+bool isPowerOfTwo(int n)
+{
+   if(n==0)
+   return false;
+  
+   return (ceil(log2(n)) == floor(log2(n)));
+}
+  
+// Driver program
+int main()
+{ int N;
+  
+std::cout << "N=";
+    cin >> N;
+    isPowerOfTwo(N)? cout<<"Yes"<<endl: cout<<"No"<<endl;
+   
+  
+    return 0;
+}
