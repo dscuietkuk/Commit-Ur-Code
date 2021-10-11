d69f149509261fb81dce11723bc808c7add78b95
ShivamSaini09 <92246594+ShivamSaini09@users.noreply.github.com>
2021-10-11 11:25:59 +0530

    Create Shivam-Saini.cpp

---
+++ Day - 1/Shivam-Saini.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t, n;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> n;
+        int x = 1;
+        while (x < n)
+        {
+            x += x;
+        }
+        if (x == n || n == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+}
