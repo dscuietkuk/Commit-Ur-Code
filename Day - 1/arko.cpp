1fd4432fddef72da198382f806b451b900e4d3f0
Arkajyoti Saha <91637787+s-arko@users.noreply.github.com>
2021-10-11 22:18:22 +0530

    Update arko.cpp

--- Day - 1/arko.cpp
+++ Day - 1/arko.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int int64_t
-int32_t main() {
+
+int main() {
     int t;
     cin>>t;
     while(t--){
-        int n;
+        long long n;
         cin>>n;
         if(n>=1){
             if((n&(n-1)) == 0) cout<<"YES";
             else cout<<"NO";
             cout<<endl;
         }
-        else cout<<"NO";    
+        else cout<<"NO\n";
     }
 
     return 0;
