6ce743be22957963556e4cb9ba82a01a8c3cad93
pallavi2043 <72869873+pallavi2043@users.noreply.github.com>
2021-10-11 17:26:40 +0530

    Update pallavi.cpp

--- Day - 1/pallavi.cpp
+++ Day - 1/pallavi.cpp
@@ -1,29 +1,26 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-int main()
-{
-    int t, n;
+bool isPowerof2(long long N){
+    if(N==0)
+    return false;
+    return ((N&(N-1))==0);
+}
+string kangaroojump(long long N){
+    if(isPowerof2(N))
+    return "YES";
+    else 
+    return "NO";
+}
+int main(){
     int t;
-    long long int n;
-    cin >> t;
-    for (int i = 0; i < t; i++)
-    {
-        cin >> n;
-        int x = 1;
-        long long int x = 1;
-        while (x < n)
-        {
-            x += x;
-        }
-        if (x == n || n == 0)
-        if (x == n )
-        {
-            cout << "YES" << endl;
-        }
-        else
-        {
-            cout << "No" << endl;
-        }
+    long long n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        cout<<kangaroojump(n)<<endl;
     }
+    return 0;
+
+
 }
 
