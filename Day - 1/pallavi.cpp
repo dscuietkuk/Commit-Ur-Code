7d948686542baaa5adbd672ad1a127228f7218ff
pallavi2043 <72869873+pallavi2043@users.noreply.github.com>
2021-10-11 17:12:15 +0530

    Update pallavi.cpp

--- Day - 1/pallavi.cpp
+++ Day - 1/pallavi.cpp
@@ -1,18 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int main(){
-   int t;
-   cin>>t;
-   while(t--){
-       long long n;
-       cin>>n;
-       
-       if((n & (n-1)) ==0)
-       cout<<"YES"<<endl;
-       else
-       cout<<"NO"<<endl;
-   }
-   return 0;
+int main()
+{
+    int t, n;
+    int t;
+    long long int n;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> n;
+        int x = 1;
+        long long int x = 1;
+        while (x < n)
+        {
+            x += x;
+        }
+        if (x == n || n == 0)
+        if (x == n )
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
 }
 
