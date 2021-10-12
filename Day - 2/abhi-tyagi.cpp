e07e94e9fd14d3750a7c0f1f907f11993c6a93ee
abhityagi09 <61769908+abhityagi09@users.noreply.github.com>
2021-10-12 20:16:24 +0530

    Update abhi-tyagi.cpp

--- Day - 2/abhi-tyagi.cpp
+++ Day - 2/abhi-tyagi.cpp
@@ -7,16 +7,11 @@ int main() {
     while(T--) {
         int N=0, x=0;
         cin>>N;
-        int a[N];
-        int b[N];
+        string a,b;
+        cin>>a;
+        cin>>b;
         for(int i=0;i<N;++i) {
-            cin>>a[i];
-        }
-        for(int i=0;i<N;++i) {
-            cin>>b[i];
-        }
-        for(int i=0;i<N;++i) {
-            if((a[i]&b[i])==1) {
+            if((a[i] == '1') && (b[i] == '1')){
                 x=1;
                 break;
             }
