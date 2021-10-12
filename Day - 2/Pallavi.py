0429cb6bad2e7e71ebdc8252ac7bd64ceda6ad50
pallavi2043 <72869873+pallavi2043@users.noreply.github.com>
2021-10-12 15:34:39 +0530

    Create Pallavi.py

---
+++ Day - 2/Pallavi.py
@@ -0,0 +1,15 @@
+def possibility(cols,row1,row2):
+    for i in range (cols):
+        if (row1[i] == '1' and row2[i] == '1'):
+            return False
+    return True
+
+
+for t in range(int(input())):
+    Cols = int(input())
+    row1 = input()
+    row2 = input()
+    if (possibility(cols,row1,row2)):
+        print('YES')
+    else:
+        print('NO')
