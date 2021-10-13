3d1f137d2c88035ef8c6fd35d65d51c0c2f06204
SUNITAGHG <78541549+SUNITAGHG@users.noreply.github.com>
2021-10-13 19:37:57 +0530

    Create day3.py

---
+++ Day - 3/day3.py
@@ -0,0 +1,9 @@
+for i in range(0,int(input())):
+    n=int(input())
+    m=int(input())
+    if(n==1 or m==1):
+        print("YES")
+    elif(n==2 and m==2):
+        print("YES")
+    else:
+        print("NO")
