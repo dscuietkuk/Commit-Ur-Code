7be2192292093b25a2ce2b4e949f270d9777048d
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2021-10-13 13:50:55 +0530

    Update Leevesh-Kamboj.py

--- Day - 3/Leevesh-Kamboj.py
+++ Day - 3/Leevesh-Kamboj.py
@@ -1,6 +1,7 @@
 for _ in range(int(input())):
-    n = int(input())
-    m = int(input())
+    d = input().split()
+    n = int(d[0])
+    m = int(d[1])
     if n == 1 or m == 1 or (n == 2 and m == 2): 
         print("YES") 
     else:
