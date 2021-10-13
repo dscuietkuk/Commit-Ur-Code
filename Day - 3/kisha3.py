0d3d0d5bb54929cef3d1116d63629f4dbb210245
kishaaggarwal32 <90393196+kishaaggarwal32@users.noreply.github.com>
2021-10-13 22:43:23 +0530

    Add files via upload

--- Day - 3/kisha3.py
+++ Day - 3/kisha3.py
@@ -3,8 +3,9 @@ for i in range (0,t):
     str = input()
     lis = str.split(' ')
     if lis[0] == '1' or lis[1] == '1':
-        print('yes')
+        print('YES')
     elif lis[0] == '2' and lis[1] == '2':
-        print('yes')
+        print('YES')
     else :
-        print('no')
+        print('NO')
+    
