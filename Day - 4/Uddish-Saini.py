3be6f7916b043b7c893e948d96a809fd13525a09
Uddish-Saini <92246725+Uddish-Saini@users.noreply.github.com>
2021-10-14 10:56:41 +0530

    Add files via upload

---
+++ Day - 4/Uddish-Saini.py
@@ -0,0 +1,16 @@
+# Function to check if the two strings are rotations of each other
+def rotation(s1,s2):
+    s1 = s1+s1
+    if (s2 in s1):
+        return True
+    else:
+        return False
+
+# Loop for Execution
+for t in range(int(input())):
+    s1 = input()
+    s2 = input()
+    if(rotation(s1,s2)):
+        print("YES")
+    else:
+        print("NO")
