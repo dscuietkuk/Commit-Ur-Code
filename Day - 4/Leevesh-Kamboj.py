6130a551b55852039c6437cba584f86e58c3cc41
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2021-10-14 16:21:12 +0530

    Update Leevesh-Kamboj.py

--- Day - 4/Leevesh-Kamboj.py
+++ Day - 4/Leevesh-Kamboj.py
@@ -1,7 +1,7 @@
 for _ in range(int(input())):
     s1 = input()
     s2 = input()
-    if (s1 * 2).count(s2): 
+    if s1 == s2 and (s1 * 2).count(s2): 
         print("YES") 
     else:
         print("NO")
