94def39b15c2e8377e9bf49bbfb8d1c608e08eb0
subham singh rawat <67608747+SubhamSRawat@users.noreply.github.com>
2021-10-14 20:53:03 +0530

    Update subham-rawat.cpp

--- Day - 4/subham-rawat.cpp
+++ Day - 4/subham-rawat.cpp
@@ -7,6 +7,7 @@ int main(){
     while(t--){
         string s1 , s2;
         cin>>s1>>s2;
+        if(s1.length() != s2.length()) {cout<<"NO"; break;}
         string s3 = s1 + s1;
         if(s3.find(s2) == string::npos) cout<<"NO";
         else cout<<"YES";
