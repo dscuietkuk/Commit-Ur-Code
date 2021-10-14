da7e334f44b83496516db4525a4c10b48aa5bc90
Alok <61348971+alokbhr@users.noreply.github.com>
2021-10-14 19:52:04 +0530

    Create ALOK-04.py
    rotation check program

---
+++ Day - 4/ALOK-04.py
@@ -0,0 +1,24 @@
+def checkRotations(s1, s2):
+    size1 = len(s1)
+    size2 = len(s2)
+    temp = ''
+    if size1 != size2:
+        return 0
+    temp = s1 + s1
+ 
+    
+    #string.count returns the number of occurrences of
+    if (temp.count(s2)):
+        return 1
+    else:
+        return 0
+ 
+
+print('s1')
+s1=input()
+print('s2')
+s2=input()
+if checkRotations(s1, s2):
+    print ("YES")
+else:
+    print ("NO")
