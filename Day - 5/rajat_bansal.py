323290fcb2f4a0e54747a539d92d377941f58b9a
Rajat <rajatbansal884@gmail.com>
2021-10-15 22:34:37 +0530

    Day 5

---
+++ Day - 5/rajat_bansal.py
@@ -0,0 +1,17 @@
+for test_case in range(int(input())):
+    n, k = map(int, input().split())
+    points = sorted(list(map(int, input().split())))
+    print(points)
+    police = 0
+    saved = 0
+    while points:
+        points[-1] += 1
+        if points[-1] == n:
+            saved += 1
+            points.pop()
+
+        police += 1
+        while police in points:
+            points.remove(police)
+
+    print(saved)
