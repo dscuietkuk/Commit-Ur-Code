edb0a12c3183c8ae92b6aeaad73b4a1bc37f8fe4
Tanishk Batheja <67550749+tanishkbatheja22@users.noreply.github.com>
2021-10-16 00:50:19 +0530

    Update Tanishk-Batheja.cpp

--- Day - 6/Tanishk-Batheja.cpp
+++ Day - 6/Tanishk-Batheja.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int maxCracker(int p,int a,int b,int c,int x,int y){
+int maxCracker(long long p,long long a,long long b,long long c,long long x,long long y){
     int anar=b+x*a;
     int rocket=c+y*a;
     int m=min(anar,rocket);
@@ -12,7 +12,7 @@ int main() {
     int t;
     cin>>t;
     while(t--){
-        int p,a,b,c,x,y;
+        long long p,a,b,c,x,y;
         cin>>p>>a>>b>>c>>x>>y;
        cout<<maxCracker(p,a,b,c,x,y)<<endl;
         
