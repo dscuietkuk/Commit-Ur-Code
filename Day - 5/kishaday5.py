75fad1d4f19ce1297889edc4a86dfa9877d5deac
kishaaggarwal32 <90393196+kishaaggarwal32@users.noreply.github.com>
2021-10-15 19:21:59 +0530

    Create kishaday5.py

---
+++ Day - 5/kishaday5.py
@@ -0,0 +1,16 @@
+t = int(input())
+for i in range(0,t):
+    val = input().split(' ')
+    n = int(val[0])
+    k = int(val[1])
+    lis = input().split(' ')  
+    for i in range(0, k):
+        lis[i] = int(lis[i])
+    lis.sort()
+    lis.reverse()  
+    curr = 0
+    counter = 0
+    while(lis[counter]>curr):
+        curr = curr + (n-lis[counter]) 
+        counter = counter + 1
+    print(counter)
