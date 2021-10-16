acf040e8b53d30683d21702414aa6028e7371781
Rohan Saini <83878702+rohansaini886@users.noreply.github.com>
2021-10-16 09:45:58 +0530

    Update Rohan-Saini.py

--- Day - 6/Rohan-Saini.py
+++ Day - 6/Rohan-Saini.py
@@ -2,9 +2,9 @@ for _ in range(int(input())):
     p, a, b, c, x, y = map(int, input().split(" "))
     total_1 = (x * a) + b
     total_2 = (y * a) + c
-
+    maxi = 0
     if total_1 <= total_2:
-        ans = p // total_1
+        maxi = p // total_1
     else:
-        ans = p // total_2
-    print(ans)
+        maxi = p // total_2
+    print(maxi)
