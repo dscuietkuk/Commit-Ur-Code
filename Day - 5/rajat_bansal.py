11286d9c6143e1b793eb961c0b27f7c644051d85
Rajat <rajatbansal884@gmail.com>
2021-10-16 22:57:13 +0530

    Day 6

--- Day - 5/rajat_bansal.py
+++ Day - 5/rajat_bansal.py
@@ -3,14 +3,8 @@ for test_case in range(int(input())):
     points = sorted(list(map(int, input().split())))
     police = 0
     saved = 0
-    while points:
-        points[-1] += 1
-        if points[-1] == n:
-            saved += 1
-            points.pop()
-
-        police += 1
-        while police in points:
-            points.remove(police)
-
+    while points[-1] > police and saved < k:
+        police = police + n - points[-1]
+        saved += 1
+        points.pop()
     print(saved)
