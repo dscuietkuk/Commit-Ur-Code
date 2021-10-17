50ea73bafb5466b9d313a169484baaae05f1e882
kishaaggarwal32 <90393196+kishaaggarwal32@users.noreply.github.com>
2021-10-17 22:36:24 +0530

    Update kisha7.py

--- DAY - 7/kisha7.py
+++ DAY - 7/kisha7.py
@@ -1,23 +1,17 @@
-def gcd(p,q):
-    if p<q:
-        p,q = q,p
-    while q != 0:
-        p, q = q, p%q
-    return p
-
-def check(j, l, r):
-    for i in range(l, r+1):
-        if gcd(i, j) != 1:
+def prime(j):
+    for i in range(2, int(j**(0.5)+1)):
+        if j%i == 0:
             return False
     return True        
+
+def check(j):
+    while(1):
+        if prime(j):
+            return j
+        j = j+1
+
 t = int(input())
 for i in range(t):
     val = input().split()
     l, r = int(val[0]), int(val[1])
-    k = r-l if r-l>2 else k=2
-    while(1):
-        if k == l: k = r
-        if check(k, l, r):
-            print(k)
-            break    
-        k = k+1
+    print(check(r+2))
