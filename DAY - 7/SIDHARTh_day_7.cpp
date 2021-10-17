714ba6d662350d68f69490b751db385dc7027bf0
00-00-0000 <92293535+00-00-0000@users.noreply.github.com>
2021-10-17 23:11:33 +0530

    Create SIDHARTh_day_7.cpp
    day 7 submitted

---
+++ DAY - 7/SIDHARTh_day_7.cpp
@@ -0,0 +1,17 @@
+/*
+   Author:Sidharth
+   Task : Day_7
+*/
+
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int l,r;
+        cin>>l>>r;
+        cout<<"1000045201<<endl;
+    }
+    return 0;
+}
