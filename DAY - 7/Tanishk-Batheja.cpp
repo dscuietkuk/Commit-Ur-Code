8e3206ade656e526301f3bcc362da9ef984eeeef
Tanishk Batheja <67550749+tanishkbatheja22@users.noreply.github.com>
2021-10-17 13:24:31 +0530

    Create Tanishk-Batheja.cpp

---
+++ DAY - 7/Tanishk-Batheja.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int l,r;
+        cin>>l>>r;
+        cout<<"1000003"<<endl;
+    }
+    return 0;
+}
