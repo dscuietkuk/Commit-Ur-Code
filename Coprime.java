029d04e05c6e5237703e46c99f4eb178d492836f
Utkarsh Bhardwaj <61777396+UtkarshBhardwaj123@users.noreply.github.com>
2021-10-18 00:23:39 +0530

    Update Coprime.java

--- Coprime.java
+++ Coprime.java
@@ -11,57 +11,10 @@ public class Coprime
 		while(t>0){
 		    str=br.readLine().split(" ");
 		    int l=Integer.parseInt(str[0]),r=Integer.parseInt(str[1]);
-		    int i=2;
-		    boolean bol=true;
-		    while(i<=l){
-		        int j=l;
-		        bol=true;
-		         while(j<=r){
-		            if(gcd(i,j)!=1){
-		                bol=false;
-		                break;
-		            }
-		            j++;
-		         }
-		         if(bol){
-		           bw.write(i+"\n");
-		           break;
-		         }
-		         i++;
-		    }
-		    if(!bol){
-		      i=r+1;
-		      while(!bol){
-		          int j=l;
-		          while(j<=r){
-		            if(gcd(i,j)!=1){
-		                bol=true;
-		                break;
-		            }
-		            j++;
-		          }
-		          if(!bol){
-		            bw.write(i+"\n");
-		            break;
-		          }
-		          bol=false;
-		          i++;
-		      }
-		    }
+		    bw.write("1000003\n");
 		    t--;
 		}
 		bw.flush();
 	}
-	public static int gcd(int m, int n){
-		        if(m==1||n==1)
-		           return 1;
-		        else if(m>n)
-		           return (gcd(n,m));
-		        else if(m==0)
-		           return n;
-		        else if(n==0)
-		           return m;
-		        else
-		           return (gcd(m,n%m));
-        }
+	
 }
